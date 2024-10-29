016b712276bf34e8665b3906e902d46665078950
Alberto <albertoromeropino2004@gmail.com>
2024-10-29 09:37:00 +0100

    enemigoEmpezar

--- Assets/SCRIPT/Enemigo_movement.cs
+++ Assets/SCRIPT/Enemigo_movement.cs
@@ -9,6 +9,9 @@ public class Enemigo_movement : MonoBehaviour
     public float speed = 4f;
     private bool isChasing;
     private bool miraDerecha = true;
+    private EnemyState enemyState;
+
+    public float attackRange = 2;
 
     private Rigidbody2D rb;
     public Transform player;
@@ -23,40 +26,60 @@ public class Enemigo_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    if(isChasing == true){
-
-        if(player.position.x > transform.position.x && !miraDerecha ||
-        player.position.x < transform.position.x && miraDerecha)
+        if (isChasing == true)
         {
-            Flip();
+            Chase();
+        }
+        else if(enemyState == EnemyState.Attacking)
+        {
 
         }
-        Vector2 direction = (player.position - transform.position).normalized;
-        rb.velocity = direction * speed;
-    } else {
-        rb.velocity = rb.velocity * 0f;
-    }    
+        else
+        {
+            rb.velocity = rb.velocity * 0f;
+        }
 
     }
 
-void Flip()
-{
-    miraDerecha = !miraDerecha;
-    transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-}
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.gameObject.tag == "Player")
-        if(player == null)
-        {
-            player = collision.transform;
-        }
+    void Chase()
+    {
+        if (player.position.x > transform.position.x && !miraDerecha ||
+            player.position.x < transform.position.x && miraDerecha)
+            {
+                Flip();
+
+            }
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.velocity = direction * speed;
+    }
+
+    void Flip()
+    {
+        miraDerecha = !miraDerecha;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            if (player == null)
+            {
+                player = collision.transform;
+            }
         isChasing = true;
     }
 
-    private void OnTriggerExit2D(Collider2D collision){
-        if(collision.gameObject.tag == "Player")
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
         {
-        isChasing = false;
+            isChasing = false;
         }
+    }
 }
+
+public enum EnemyState
+{
+    Idle,
+    isChasing,
+    Attacking
 }
