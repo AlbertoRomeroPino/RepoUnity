ad6ed01fdae80580f0ff0107438fd3566365a3dc
Trabuco03 <90504048+Trabuco03@users.noreply.github.com>
2024-10-29 10:32:09 +0100

    intento ataque

--- Assets/SCRIPT/MOMIMIENTO.cs
+++ Assets/SCRIPT/MOMIMIENTO.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MOMIMIENTO : MonoBehaviour
@@ -9,10 +10,22 @@ public float speed = 5;
 public int facingDirection = 1;
 
 public Rigidbody2D rb;
-public Animator anim;    
+public Animator anim; 
+private bool isKnocKedBack;
+
+public Player_combat player_Combat;
+
+private void Update()
+{
+    if(Input.GetButtonDown("Slash"))
+    {
+        player_Combat.Attack();
+    }
+}
 
     // Fixed Update is called 50X frame
-    void FixedUpdate()
+    void FixedUpdate(){
+    if (isKnocKedBack == false)
     {
 
         float horizontal = Input.GetAxis("Horizontal");
@@ -20,6 +33,7 @@ public Animator anim;
 
         if(horizontal > 0 && transform.localScale.x < 0 ||
            horizontal < 0 && transform.localScale.x > 0 )
+    
            {
                 Flip();
            }
@@ -27,14 +41,15 @@ public Animator anim;
         anim.SetFloat("horizontal", Mathf.Abs(horizontal));
         anim.SetFloat("vertical", Mathf.Abs(vertical));
 
-
+    
         rb.velocity = new Vector2(horizontal, vertical) * speed;
 
-        
+    
     }
     void Flip()
     {
         facingDirection *= -1;
         transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
+}
 }
