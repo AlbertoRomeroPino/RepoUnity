168bd19a45da8a432f88537be69bd1fa0d037fc7
Alberto <albertoromeropino2004@gmail.com>
2024-10-30 09:34:14 +0100

    Enemigo empuja

--- Assets/SCRIPT/Enemy_Movement.cs
+++ Assets/SCRIPT/Enemy_Movement.cs
@@ -84,7 +84,7 @@ public class Enemy_Movement : MonoBehaviour
                 ChangeState(EnemyState.Attacking);
             }
 
-            else if (Vector2.Distance(transform.position, player.position) > attackRange)
+            else if (Vector2.Distance(transform.position, player.position) > attackRange && enemyState != EnemyState.Attacking)
             {
                 ChangeState(EnemyState.isChasing);
             }
---
+++ Assets/SCRIPT/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5;
+    public int facingDirection = 1;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+    private bool isKnocKedBack;
+    public Player_combat player_Combat;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Slash"))
+        {
+            player_Combat.Attack();
+        }
+    }
+
+    // Fixed Update is called 50X frame
+    void FixedUpdate()
+    {
+        if (isKnocKedBack == false)
+        {
+
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            if (horizontal > 0 && transform.localScale.x < 0 ||
+               horizontal < 0 && transform.localScale.x > 0)
+
+            {
+                Flip();
+            }
+
+            anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+            anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+
+            rb.velocity = new Vector2(horizontal, vertical) * speed;
+
+
+        }
+        void Flip()
+        {
+            facingDirection *= -1;
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }}
+
+    public void KnockBack(Transform enemy, float force, float StunTime)
+    {
+        isKnocKedBack = true;
+        Vector2 direction = (transform.position - enemy.position).normalized;
+        rb.velocity = direction * force;
+        StartCoroutine(KnockbackCounter(StunTime));
+    }
+
+    IEnumerator KnockbackCounter(float stunTime)
+    {
+        yield return new WaitForSeconds(stunTime);
+        rb.velocity = Vector2.zero;
+        isKnocKedBack = false;
+    }
+
+}
